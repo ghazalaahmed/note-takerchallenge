c03c0a0f89119b90ba654e72b37c9f2580796b5c
Ghazala Ahmed <ghazala599@gmail.com>
2022-11-30 22:18:06 -0800

    Added to index.js file

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
-
 const PORT = process.env.port || 3001;
-
 const app = express();
+const fs = require("fs");
+const uniqId = require("uniqid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
-//Add routes in here
-//api/notes
+//api routes
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = uniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
 //HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+//Delete Note
+app.delete("/notes/:id", (req, res) => {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
